761ccad5ad7cbd7541035881d2b37245c4600a47
Jost0phe <48672642+Jost0phe@users.noreply.github.com>
2020-10-13 14:10:03 +0200

    Profil logic
    timestamp issue

--- app/src/main/java/com/example/gsbandroidapp/ProfilActivity.java
+++ app/src/main/java/com/example/gsbandroidapp/ProfilActivity.java
@@ -1,14 +1,80 @@
 package com.example.gsbandroidapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfilActivity extends AppCompatActivity {
+
+    private DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference("fiches");
+    private Long maxId;
+    private TextView textUser, textDate, textCount;
+    private Button btnMenu, btnConsult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+        textUser = findViewById(R.id.textViewUser);
+        textDate = findViewById(R.id.textViewDate);
+        textCount = findViewById(R.id.textViewCount);
+        btnMenu = findViewById(R.id.btnMenu);
+        btnConsult = findViewById(R.id.btnConsult);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent v = new Intent(ProfilActivity.this, MenuActivity.class);
+                startActivity(v);
+                finish();
+            }
+        });
+
+        btnConsult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent v = new Intent(ProfilActivity.this, ConsultActivity.class);
+                startActivity(v);
+                finish();
+            }
+        });
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        final String userMail = user.getEmail();
+        String userId = user.getUid();
+        DatabaseReference newRef = mRootRef.child(userId);
+        final String date = String.valueOf(user.getMetadata().getCreationTimestamp());
+        newRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                maxId = dataSnapshot.getChildrenCount();
+                String maxIdString = maxId.toString();
+                textUser.setText(userMail);
+                textDate.setText(date);
+                textCount.setText(maxIdString);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
     }
 }
